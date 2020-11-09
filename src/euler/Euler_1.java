ae8733ce7dbcb9f6b6416cf2f7a7589e00d0cc0d
abryantr <andrew.richards67@gmail.com>
2020-11-08 20:53:47 -0500

    solved prob 2, added comments

--- src/euler/Euler_1.java
+++ src/euler/Euler_1.java
@@ -1,12 +1,16 @@
 package euler;
 
 /*
+ * Problem 1: Multiples of 3 and 5
+ * 
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  * 
  * Find the sum of all the multiples of 3 or 5 below 1000.
 */
+
 public class Euler_1 {
 	
+	//returns true if input is a multiple of 3 and 5
 	public static boolean isMultiple(int input) {
 		if(input % 3 == 0 || input % 5 == 0) return true;
 		return false;
--- src/euler/Main.java
+++ src/euler/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Problem 1: " + Euler_1.output());
+		System.out.println("Problem 2: " + Euler_2.output());
 	}
 }
