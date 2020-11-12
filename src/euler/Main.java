86308b0bc72c19545ab193069a9a1404a0e6c2a3
abryantr <andrew.richards67@gmail.com>
2020-11-12 17:43:28 -0500

    added clarifying comments to main

--- src/euler/Main.java
+++ src/euler/Main.java
@@ -3,9 +3,20 @@ package euler;
 public class Main {
 
 	public static void main(String[] args) {
+		//Problem 1: Multiples of 3 and 5
 		//System.out.println("Problem 1: " + Euler_1.output());
+		//233168
+		
+		//Problem 2: Even Fibonacci numbers
 		//System.out.println("Problem 2: " + Euler_2.output());
+		//4613732
+		
+		//Problem 3: Largest prime factor
 		//System.out.println("Problem 3: " + Euler_3.output());
+		//6857
+		
+		//Problem 4: Largest palindrome product
 		System.out.println("Problem 4: " + Euler_4.output());
+		//906609
 	}
 }
